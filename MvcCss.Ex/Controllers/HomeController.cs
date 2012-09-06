29cd39d9949311359b87cc84a8d7107a2e2b5994
chaiwat-kh <chaiwat.kh@carpass.co.th>
2012-09-06 15:21:00 +0700

    add CSS3 project

---
+++ MvcCss.Ex/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCss.Ex.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter3()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter4()
+        {
+            return View();
+        }
+        public ActionResult Chapter41()
+        {
+            return View();
+        }
+        public ActionResult Chapter5()
+        {
+            return View();
+        }
+        public ActionResult Chapter51()
+        {
+            return View();
+        }
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+
+        public ActionResult PricingPage()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter61Form()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter62FieldSets()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter63Inline()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter81InlineBlock()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter82Float()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter83Position()
+        {
+            return View();
+        }
+        public ActionResult Chapter84Gallery()
+        {
+            return View();
+        }
+        public ActionResult Chapter9TwoColPosition()
+        {
+            return View();
+        }
+
+        public ActionResult Chapter9TwoColFloat()
+        {
+            return View();
+        }
+        public ActionResult Chapter9ThreeColFloat()
+        {
+            return View();
+        }
+        public ActionResult Chapter9ThreeColWideFooter()
+        {
+            return View();
+        }
+        public ActionResult Chapter9Responsive()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ MvcCss.Ex/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcCss.Ex.MvcApplication" Language="C#" %>
